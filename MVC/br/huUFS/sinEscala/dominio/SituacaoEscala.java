c4c75ab39cde43da2189abf9b78a445b3454f9ad
pfslima <pfslima@PabloLima>
2018-02-05 23:29:07 -0300

    Pacote de Dominio - Unidade e Situação de Escala

---
+++ MVC/br/huUFS/sinEscala/dominio/SituacaoEscala.java
@@ -0,0 +1,43 @@
+/*
+ * @author Pablo Lima
+ * Classe de domínio que define os status de uma escala
+ * Por boas práticas, todas as operações em uma escala devem ser feitas utilizando o nome das variáveis
+ * e não o seu conteúdo.
+ */
+
+package br.huUFS.sinEscala.dominio;
+
+import javax.persistence.Id;
+
+public class SituacaoEscala {
+	
+	@Id
+	public int id;
+	
+	public static final int CRIADA = 1;
+	public static final int EDITADA = 2;
+	public static final int SUBMETIDA = 3;
+	public static final int EM_ANALISE_CHEFIA = 4;
+	public static final int DEVOLVIDA_PARA_AJUSTES = 5;
+	public static final int HOMOLOGADA = 6;
+	public static final int EXECUTADA = 7;
+	public static final int FINALIZADA = 8;
+	
+	public String descricao;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
---
+++ MVC/br/huUFS/sinEscala/dominio/Unidade.java
@@ -0,0 +1,114 @@
+/*
+ * @author Pablo Lima
+ */
+
+package br.huUFS.sinEscala.dominio;
+
+import java.util.List;
+
+import javax.persistence.Id;
+
+
+public class Unidade {
+	
+	
+	/*Nome da Unidade*/
+	public String nome;
+	
+	/*Descricao do departamento*/
+	public String descricao;
+	
+	@Id
+	public int id;
+	
+	/*Lista dos profissionais da Unidade*/
+	public List<Profissional> colaboradores;
+	
+	/*Chefe da Unidade*/
+	public Profissional subChefe;
+	
+	/*Sigla da Unidade*/
+	public String sigla;
+	
+	/*Quantidade de Leitos que a Unidade dispõe*/
+	public int quantidadeLeitos;
+	
+	/*Atributo que define se a Unidade está ativa no HU*/
+	public boolean isAtiva;
+	
+	/*Atributo que define se a Unidade tratada é de internação*/
+	public boolean isInternacao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public List<Profissional> getColaboradores() {
+		return colaboradores;
+	}
+
+	public void setColaboradores(List<Profissional> colaboradores) {
+		this.colaboradores = colaboradores;
+	}
+
+	public Profissional getSubChefe() {
+		return subChefe;
+	}
+
+	public void setSubChefe(Profissional subChefe) {
+		this.subChefe = subChefe;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public int getQuantidadeLeitos() {
+		return quantidadeLeitos;
+	}
+
+	public void setQuantidadeLeitos(int quantidadeLeitos) {
+		this.quantidadeLeitos = quantidadeLeitos;
+	}
+
+	public boolean isAtiva() {
+		return isAtiva;
+	}
+
+	public void setAtiva(boolean isAtiva) {
+		this.isAtiva = isAtiva;
+	}
+
+	public boolean isInternacao() {
+		return isInternacao;
+	}
+
+	public void setInternacao(boolean isInternacao) {
+		this.isInternacao = isInternacao;
+	}
+	
+}
